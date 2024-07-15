4fc5d31abf60a1427de69d15f2ddc82b7c7bc57c
chenqi777 <996504914@qq.com>
2024-07-15 14:58:01 +0800

    fix: 修复build错误

--- android/src/main/java/org/wonday/aliyun/push/MIUIUtils.java
+++ android/src/main/java/org/wonday/aliyun/push/MIUIUtils.java
@@ -87,7 +87,7 @@ public class MIUIUtils {
                 PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
                 Notification.Builder builder = new Notification.Builder(context)
-                                    .setContentTitle(context.getString(R.string.app_name))
+                                    .setContentTitle(" ")
                                     .setContentText("您有"+Integer.toString(badgeNumber)+"条未读消息")
                                     .setWhen(System.currentTimeMillis())
                                     .setContentIntent(contentIntent)
